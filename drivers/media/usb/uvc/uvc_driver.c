82b7107ba3e2b658b8ffcdf1e590d946a2d65626
Android Linux Media <android.linuxmedia@gmail.com>
2017-05-26 15:21:52 +0530

    feature(rhomb): USB video class device support
    This commit enables USB video class device support. We have added USB
    camera support of Alcor corporation.
    In addition to that in this commit we have restricted maximum cpu
    frequency to 1Ghz as per Exynos4412 datasheet.
    
    Signed-off-by: Android Linux Media <android.linuxmedia@gmail.com>

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -2245,6 +2245,14 @@ static struct usb_device_id uvc_ids[] = {
 	  .bInterfaceSubClass	= 1,
 	  .bInterfaceProtocol	= 0,
 	  .driver_info		= UVC_QUIRK_PROBE_MINMAX },
+        { .match_flags          = USB_DEVICE_ID_MATCH_DEVICE
+                                | USB_DEVICE_ID_MATCH_INT_INFO,
+          .idVendor             = 0x058f,
+          .idProduct            = 0xd501,
+          .bInterfaceClass      = USB_CLASS_VIDEO,
+          .bInterfaceSubClass   = 1,
+          .bInterfaceProtocol   = 0,
+          .driver_info          = UVC_QUIRK_PROBE_MINMAX },
 	/* Dell XPS m1530 */
 	{ .match_flags		= USB_DEVICE_ID_MATCH_DEVICE
 				| USB_DEVICE_ID_MATCH_INT_INFO,
